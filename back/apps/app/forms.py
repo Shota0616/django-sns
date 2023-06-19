a7dd549296b73c835f1f5d8b354865e95173e51e
ShotaIsoda <ju.oh0616@gmail.com>
2023-06-19 23:56:59 +0900

    20230619

--- back/apps/app/forms.py
+++ back/apps/app/forms.py
@@ -4,5 +4,28 @@ from django import forms
 from .models import Tweet
 
 
-class TweetForm(forms.Form):
-    pass
+class TweetForm(forms.ModelForm):
+    text = forms.CharField(max_length=300,
+        widget=forms.TextInput(
+        attrs={'class':'form-control'}))
+
+    class Meta:
+        model = Tweet
+        fields = ['text']
+
+    # djangoデフォルトのバリデーションメッセージを表示したくないので、required = False
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.required = False
+
+    def clean_text(self):
+        text = self.cleaned_data.get('text')
+        if not text:
+            raise forms.ValidationError('何も入力されていません。')
+        return text
+
+    # def save(self, user_data):
+    #     user_data.text = self.cleaned_data['text']
+    #     user_data.save()
+    #     return user_data
--- back/apps/app/views.py
+++ back/apps/app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import TemplateView
 from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -17,13 +17,13 @@ class IndexView(TemplateView):
 class TweetListView(View):
     def get(self, request, *args, **kwargs):
         tweets = Tweet.objects.all()  # 全てのツイートを取得
-        return render(request, 'tweets/list.html', {'tweets': tweets})
+        return render(request, 'app/list.html', {'tweets': tweets})
 
 # 新しいTweetを作成
 class TweetCreateView(View):
     def get(self, request, *args, **kwargs):
-        form = TweetForm()  # フォームを初期化
-        return render(request, 'tweets/create.html', {'form': form})
+        form = TweetForm()
+        return render(request, 'app/tweet_create.html', {'form': form})
 
     def post(self, request, *args, **kwargs):
         form = TweetForm(request.POST)
@@ -31,22 +31,22 @@ class TweetCreateView(View):
             tweet = form.save(commit=False)
             tweet.user = request.user  # ログインしているユーザーを設定
             tweet.save()
-            return redirect('tweets:list')  # Tweetの一覧ページにリダイレクト
+            return redirect('profile')  # Tweetの一覧ページにリダイレクト
         else:
-            return render(request, 'tweets/create.html', {'form': form})
+            return render(request, 'app/tweet_create.html', {'form': form})
 
 # Tweetの詳細を表示
 class TweetDetailView(View):
     def get(self, request, pk, *args, **kwargs):
         tweet = get_object_or_404(Tweet, pk=pk)  # Tweetを取得、存在しない場合は404エラーを表示
-        return render(request, 'tweets/detail.html', {'tweet': tweet})
+        return render(request, 'app/detail.html', {'tweet': tweet})
 
 # Tweetを編集
 class TweetUpdateView(View):
     def get(self, request, pk, *args, **kwargs):
         tweet = get_object_or_404(Tweet, pk=pk)
         form = TweetForm(instance=tweet)  # フォームを初期化、初期値はtweet
-        return render(request, 'tweets/update.html', {'form': form})
+        return render(request, 'app/update.html', {'form': form})
 
     def post(self, request, pk, *args, **kwargs):
         tweet = get_object_or_404(Tweet, pk=pk)
@@ -55,13 +55,13 @@ class TweetUpdateView(View):
             form.save()
             return redirect('tweets:detail', pk=tweet.pk)  # 編集後のTweetの詳細ページにリダイレクト
         else:
-            return render(request, 'tweets/update.html', {'form': form})
+            return render(request, 'app/update.html', {'form': form})
 
 # Tweetを削除
 class TweetDeleteView(View):
     def get(self, request, pk, *args, **kwargs):
         tweet = get_object_or_404(Tweet, pk=pk)
-        return render(request, 'tweets/confirm_delete.html', {'tweet': tweet})
+        return render(request, 'app/confirm_delete.html', {'tweet': tweet})
 
     def post(self, request, pk, *args, **kwargs):
         tweet = get_object_or_404(Tweet, pk=pk)
